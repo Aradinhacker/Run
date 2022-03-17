b72bef8423353f0f275dc18a20aa309341a04e18
AradinHaker <89314157+Aradinhacker@users.noreply.github.com>
2022-03-17 20:41:52 +0500

    Update Go.py

--- Go.py
+++ Go.py
@@ -1,8 +1,6 @@
 GNU nano 6.2                 Go.py                           import os,platform
-os.system('git pull')
+
 
 bsn=platform.architecture()[0]
-if bsn=="32bit":
-    __import__("bn").reg()
 elif bsn=="64bit":
     __import__("bs").reg()
